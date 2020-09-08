7d23c9046580bc759a82a1f4da22b211e7bf1168
anower035 <faysalanower71@gmail.com>
2020-09-08 11:50:46 +0600

    true vs false

---
+++ truthy-falsy.js
@@ -0,0 +1,15 @@
+//falsy
+//0
+//"";
+//undefined;
+//null;
+//NaN;
+//truthy
+//'0', ' ',[],{},value=false;'something'
+let name=0
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
