2161f2d25ff49896ba28e569a35aae41ff55e3f4
anower035 <faysalanower71@gmail.com>
2020-09-08 12:19:13 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+// let pakhi;
+// function add(num1,num2) {
+//     console.log(num1+num2);
+//     return
+// }
+// function add(num1,num2) {
+//     console.log(num1,num2);
+// }
+// const premik={name:"shihab",age:35,address:"narayanganj"};
+// console.log(premik.gfName);
+
+// let fun=undefined;
+// console.log(fun);
+
+let ages=[9,13,14];
+console.log(ages[11]);
